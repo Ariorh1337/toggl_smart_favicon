5f7b4b0938edd4298ee83efdaf81d0a7f5b6ae29
Dmitry <35579597+Ariorh1337@users.noreply.github.com>
2019-12-28 23:31:38 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 function new_icon(text = '0:00', color = '#fff') {
-    let c = document.createElement( "canvas" );
+    let c = document.createElement("canvas"); // Используем тот же канвас
     c.height = c.width = 16;
-    let cx = c.getContext( "2d" );
-    cx.beginPath();
+    let cx = c.getContext("2d");
+    cx.beginPath();// рисуем голубенький квадратик и черный текст на нем
     cx.font = "22px";
     cx.fillStyle = color;
     cx.fillText( text, 0, 12, 16 );
     return c.toDataURL()
 }
-function add_icon (icon) {
-    var oldicons = document.querySelectorAll( 'link[rel="icon"], link[rel="shortcut icon"]' );
-    for(var i = 0; i < oldicons.length; i++) {
-        oldicons[i].parentNode.removeChild( oldicons[i] );
-    }
-    var newicon = document.createElement( "link" );
-    newicon.setAttribute( "rel", "icon" );
-    newicon.setAttribute( "href", icon );
-    document.querySelector( "head" ).appendChild( newicon );
-}
+
 setTimeout(function() {
+    var s$icon = { back: true, elm: document.querySelector( 'link[rel="shortcut icon"]').href};
 	setInterval( () => {
-		var time = document.querySelector('div > div > div[title="Add duration"]').innerText.slice(-4)
-		add_icon(new_icon(time))
-	}, 1000);
+		let time = document.querySelector('div > div > div[title="Add duration"]').innerText.slice(-4);
+		if (time !== '0:00') {
+			document.querySelector('link[rel="shortcut icon"]').href = new_icon(time);
+			s$icon.back == false;
+		} else {
+			if (s$icon.back == false) {
+				document.querySelector('link[rel="shortcut icon"]').href = s$icon.elm;
+				s$icon.back = true;
+			}
+		}
+	}, 1000, s$icon);
 }, 5000);
