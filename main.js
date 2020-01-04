8df33cce82aebcb15c280e78f6e780a6998e13de
Dmitry <35579597+Ariorh1337@users.noreply.github.com>
2020-01-04 12:29:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -14,21 +14,18 @@ setTimeout(function() {
     document.querySelector( 'link[rel="shortcut icon"]').setAttribute('rel', 'icon');
 	setInterval( () => {
 		let time = document.querySelector('div > div > div[title="Add duration"]').innerText.split(':');
+		let link = document.querySelector('link[rel="icon"]');
 		if (Number(time[0]) == 0 && Number(time[1]) == 0 && Number(time[2]) == 0) {
 			if (sicon.back == false) {
-				document.querySelector('link[rel="icon"]').href = sicon.href;
+				link.href = sicon.href;
 				sicon.back = true;
 			}
 		} else {
-			if (Number(time[1]) < 9 && Number(time[0]) == 0) {
-				document.querySelector('link[rel="icon"]').href = new_icon(time[1].slice(1) + ':' + time[2]);
-			}
-			if (Number(time[1]) > 9 && Number(time[0]) == 0) {
-				document.querySelector('link[rel="icon"]').href = new_icon(time[1] + ':' + time[2].slice(0,1));
-			}
-			if (Number(time[0]) > 0) {
-				document.querySelector('link[rel="icon"]').href = new_icon(time[0] + ':' + time[1]);
-			}
+			(Number(time[1]) < 9 && Number(time[0]) == 0) ? link.href = new_icon(time[1].slice(1) + ':' + time[2]) : false;
+			(Number(time[1]) > 9 && Number(time[0]) == 0) ? link.href = new_icon(time[1] + ':' + time[2].slice(0,1)) : false;
+			(Number(time[0]) > 0) ? link.href = new_icon(time[0] + ':' + time[1]) : false;
+			(Number(time[0]) > 9) ? link.href = new_icon(time[0] + ':' + time[1].slice(0,1)) : false;
+			(Number(time[0]) > 99) ? link.href = new_icon(time[0]) : false;
 			sicon.back = false;
 		}
 	}, 1000, sicon);
