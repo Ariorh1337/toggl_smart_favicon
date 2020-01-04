fff55b6029580e01e1968f18a13f212d8c4be5c8
Dmitry <35579597+Ariorh1337@users.noreply.github.com>
2020-01-04 08:47:10 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function new_icon(text = '0:00', color = '#fff') {
-    let c = document.createElement("canvas"); // Используем тот же канвас
+    let c = document.createElement("canvas");
     c.height = c.width = 16;
     let cx = c.getContext("2d");
-    cx.beginPath();// рисуем голубенький квадратик и черный текст на нем
+    cx.beginPath();
     cx.font = "22px";
     cx.fillStyle = color;
     cx.fillText( text, 0, 12, 16 );
@@ -13,14 +13,22 @@ setTimeout(function() {
     var sicon = { back: true, href: document.querySelector( 'link[rel="shortcut icon"]').href};
     document.querySelector( 'link[rel="shortcut icon"]').setAttribute('rel', 'icon');
 	setInterval( () => {
-		let time = document.querySelector('div > div > div[title="Add duration"]').innerText.slice(-4);
-		if (time == '0:00') {
+		let time = document.querySelector('div > div > div[title="Add duration"]').innerText.split(':');
+		if (Number(time[0]) == 0 && Number(time[1]) == 0 && Number(time[2]) == 0) {
 			if (sicon.back == false) {
 				document.querySelector('link[rel="icon"]').href = sicon.href;
 				sicon.back = true;
 			}
 		} else {
-			document.querySelector('link[rel="icon"]').href = new_icon(time);
+			if (Number(time[1]) < 9 && Number(time[0]) == 0) {
+				document.querySelector('link[rel="icon"]').href = new_icon(time[1].slice(1) + ':' + time[2]);
+			}
+			if (Number(time[1]) > 9 && Number(time[0]) == 0) {
+				document.querySelector('link[rel="icon"]').href = new_icon(time[1] + ':' + time[2].slice(0,1));
+			}
+			if (Number(time[0]) > 0) {
+				document.querySelector('link[rel="icon"]').href = new_icon(time[0] + ':' + time[1]);
+			}
 			sicon.back = false;
 		}
 	}, 1000, sicon);
